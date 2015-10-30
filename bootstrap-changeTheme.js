24ccab6b16395e3f8cfd1eacb96143ec483598e3
ilovezy <1518265554@qq.com>
2015-10-30 09:20:31 +0800

    add a default and change the links to local css files to make it load faster

--- bootstrap-changeTheme.js
+++ bootstrap-changeTheme.js
@@ -2,14 +2,14 @@
  * @Author: fhc
  * @Date:   2015-10-30 08:31:57
  * @Last Modified by:   fhc
- * @Last Modified time: 2015-10-30 08:58:28
+ * @Last Modified time: 2015-10-30 09:18:47
  */
 
 (function($) {
     'use strict';
     // thanks to this guy https://github.com/thomaspark/bootswatch
     // cerulean cosmo cyborg darkly flatly journal lumen paper readable sandstone simplex slate spacelab superhero united yeti
-    var themeNameArr = ['cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'lumen', 'paper', 'readable', 'sandstone', 'simplex', 'slate', 'spacelab', 'superhero', 'united', 'yeti'];
+    var themeNameArr = ['default', 'cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'lumen', 'paper', 'readable', 'sandstone', 'simplex', 'slate', 'spacelab', 'superhero', 'united', 'yeti'];
 
     var changeThemeStr = '';
 
@@ -23,9 +23,14 @@
     var $changeTheme = $('.changeTheme');
     var $themeLink = $('#themeLink');
 
+    function changeLinkUrl(themeName) {
+        // $themeLink.prop('href', 'http://bootswatch.com/' + themeName + '/bootstrap.min.css');
+        $themeLink.prop('href', 'bootswatch/bootstrap-' + themeName + '.css');
+    }
+
     // 如果本地有存着皮肤的话，就用存着的那个
     if (localStorage.getItem('boostrapTheme')) {
-        $themeLink.prop('href', 'http://bootswatch.com/' + localStorage.getItem('boostrapTheme') + '/bootstrap.min.css');
+        changeLinkUrl(localStorage.getItem('boostrapTheme'))
         $changeTheme.val(localStorage.getItem('boostrapTheme'));
     }
 
@@ -39,8 +44,9 @@
         $changeTheme.html(changeThemeStr);
 
         $changeTheme.on('change', function() {
-            $themeLink.prop('href', 'http://bootswatch.com/' + $(this).val() + '/bootstrap.min.css');
-            // 给localStorage 设个值
+            // $themeLink.prop('href', 'http://bootswatch.com/' + $(this).val() + '/bootstrap.min.css');
+            changeLinkUrl($(this).val())
+                // 给localStorage 设个值
             localStorage.setItem('boostrapTheme', $(this).val());
         });
 
@@ -52,8 +58,9 @@
 
         $changeTheme.html(changeThemeStr)
         $changeTheme.find('li a').on('click', function() {
-            $themeLink.prop('href', 'http://bootswatch.com/' + $(this).data('themename') + '/bootstrap.min.css');
-            // 给localStorage 设个值
+            // $themeLink.prop('href', 'http://bootswatch.com/' + $(this).data('themename') + '/bootstrap.min.css');
+            changeLinkUrl($(this).data('themename'))
+                // 给localStorage 设个值
             localStorage.setItem('boostrapTheme', $(this).data('themename'));
         });
     }
